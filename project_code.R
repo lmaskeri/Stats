5d9694ebe707113caa7d97f882cb69e0d17f59b6
Annie M. Novak <anovak9@luc.edu>
2021-04-21 21:08:19 -0500

    Added installing & loading necessary packages as well as loading cancer dataset

--- project_code.R
+++ project_code.R
@@ -1,14 +1,23 @@
-#Load necessary packages
-suppressMessages(library(TeachingDemos))
+## Load necessary packages
+#Install EDASeq package
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("EDASeq")
+#Load package
+library(EDASeq)
+#Install glmnet
+install.packages('glmnet')
+library(glmnet)
+
+## Load data
+data <- read.csv('/homes/anovak9/stats/data.csv', header = TRUE, stringsAsFactors = FALSE)
+colnames(data)[1] <- 'sample'
+str(data)
 
 
-#Load Leukemia dataset
-leukemia <- read.csv("http://web.stanford.edu/~hastie/CASI_files/DATA/leukemia_small.csv")
-#ALL = 0, AML = 1
-class <- c(rep(0,27),rep(1,11),rep(0,11),rep(1,5),rep(0,2),rep(1,2),0,rep(1,7),rep(0,6))
-#Give patients numeric ids
-id <- c(1:72)
-#Add above changes to DF leukemia 
-colnames(leukemia)<-NULL 
-leuk <- rbind(class,leukemia) 
-colnames(leuk)<-c(1:72)
+#Need to figure out if they used RPM, RPKM, FPKM, or TPM to quantify counts of a gene
+
+
+
+## Normalization
