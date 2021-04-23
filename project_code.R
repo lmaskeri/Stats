d5eb8565721aa8843251e2dd601ddd345ed35f5a
Annie M. Novak <anovak9@luc.edu>
2021-04-22 19:54:57 -0500

    Update

--- project_code.R
+++ project_code.R
@@ -9,15 +9,28 @@ library(EDASeq)
 #Install glmnet
 install.packages('glmnet')
 library(glmnet)
+library(dplyr)
+
 
 ## Load data
 data <- read.csv('/homes/anovak9/stats/data.csv', header = TRUE, stringsAsFactors = FALSE)
 colnames(data)[1] <- 'sample'
 str(data)
+summary(data)
+data[1:5,1:5]
 
-
-#Need to figure out if they used RPM, RPKM, FPKM, or TPM to quantify counts of a gene
-
-
+#Data has 801 samples, 20531 genes
+dim(data)
+#Store number of zero counts per gene
+count <- function(x){
+  sum(x == 0)
+}
+zeros <- apply(data[2:20532], MARGIN = 2, FUN = count)
+zeros <- as.data.frame(zeros)
+zeros <- as.numeric(zeros$zeros)
+summary(zeros)
+hist(zeros)
+zeros <- zeros %>% arrange(zeros)
+#keep 75% after rank 
 
 ## Normalization
