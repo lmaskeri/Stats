ef66a5f0e78c1351043c29683ffa27dac5ea1f7d
Annie M. Novak <anovak9@luc.edu>
2021-04-23 11:09:01 -0500

    Added our version of normalization and implemented -1 dummy values

--- project_code.R
+++ project_code.R
@@ -1,23 +1,16 @@
 ## Load necessary packages
-#Install EDASeq package
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("EDASeq")
-#Load package
-library(EDASeq)
-#Install glmnet
 install.packages('glmnet')
 library(glmnet)
 library(dplyr)
-
 
 ## Load data
 data <- read.csv('/homes/anovak9/stats/data.csv', header = TRUE, stringsAsFactors = FALSE)
 colnames(data)[1] <- 'sample'
 str(data)
 summary(data)
-data[1:5,1:5]
+
+
+## Pre-processing/Normalization
 
 #Data has 801 samples, 20531 genes
 dim(data)
@@ -26,11 +19,41 @@ count <- function(x){
   sum(x == 0)
 }
 zeros <- apply(data[2:20532], MARGIN = 2, FUN = count)
+#Format zero count DF
+zeros <- as.numeric(zeros)
 zeros <- as.data.frame(zeros)
-zeros <- as.numeric(zeros$zeros)
+genes <- colnames(data)[2:20532]
+rownames(zeros) <- genes
+colnames(zeros)[1] <- 'Count'
+#Look at info about zero counts
 summary(zeros)
-hist(zeros)
-zeros <- zeros %>% arrange(zeros)
-#keep 75% after rank 
+hist(zeros$Count)
+#Arrange DF so counts in ascending order
+zeros <- zeros %>% arrange(Count)
+#Keep top 75% of rows in zeros
+keep <- as.integer(20531*0.75)
+keep
+zeros2 <- zeros %>% slice(1:keep)
+dim(zeros2)
+genes_to_keep <- rownames(zeros2)
+#Subset data DF based on genes left in zeros DF
+data2 <- data[,colnames(data) %in% genes_to_keep]
+dim(data2)
+#Replace all 0 values with -1
+data2[data2 == 0] <- -1
+which(data2 == 0)
 
-## Normalization
+
+## Ordination
+
+
+
+## Training
+
+
+
+## Testing
+
+
+
+## Evaluations
