cb23e1dbf8440a1f74acb299171e7f24d846ac86
Annie M. Novak <anovak9@luc.edu>
2021-04-28 17:55:25 -0500

    Ordination plots and timed classifiers

--- project_code.R
+++ project_code.R
@@ -3,7 +3,9 @@
 # install.packages("umap") # click no to compiling from source
 # install.packages("Rtsne")
 # install.packages("caret")
-# install.packages("pROC") 
+# install.packages("pROC")
+# install.packages('ggpubr')
+# devtools::install_github("collectivemedia/tictoc")
 library(glmnet)
 library(dplyr)
 library(umap)
@@ -11,14 +13,16 @@ library(Rtsne)
 library(ggplot2)
 library(caret)
 library(pROC)
+library(ggpubr)
+library(tictoc)
 
 ## Load data
-data <- read.csv('/Users/lcmas/Desktop/statsproject/data.csv', header = TRUE, stringsAsFactors = FALSE)
-#data <- read.csv('/homes/anovak9/stats/data.csv', header = TRUE, stringsAsFactors = FALSE)
+#data <- read.csv('/Users/lcmas/Desktop/statsproject/data.csv', header = TRUE, stringsAsFactors = FALSE)
+data <- read.csv('/homes/anovak9/stats/data.csv', header = TRUE, stringsAsFactors = FALSE)
 #data <- read.csv('C:/Users/Tristan Kosciuch/OneDrive - Loyola University Chicago/Loyola/Classes/STAT 437/Final-Project/TCGA-PANCAN-HiSeq-801x20531/data.csv', header = TRUE, stringsAsFactors = FALSE)
 colnames(data)[1] <- 'sample'
-label <- read.csv('/Users/lcmas/Desktop/statsproject/labels.csv', header = TRUE, stringsAsFactors = FALSE)
-#label <- read.csv('/homes/anovak9/stats/labels.csv', header = TRUE, stringsAsFactors = FALSE)
+#label <- read.csv('/Users/lcmas/Desktop/statsproject/labels.csv', header = TRUE, stringsAsFactors = FALSE)
+label <- read.csv('/homes/anovak9/stats/labels.csv', header = TRUE, stringsAsFactors = FALSE)
 #label <- read.csv('C:/Users/Tristan Kosciuch/OneDrive - Loyola University Chicago/Loyola/Classes/STAT 437/Final-Project/TCGA-PANCAN-HiSeq-801x20531/labels.csv') #stringsAsFactors = FALSE
 colnames(label)[1] <- 'sample'
 # str(data)
@@ -26,7 +30,7 @@ colnames(label)[1] <- 'sample'
 
 
 #### Pre-processing/Normalization #### 
-
+tic('Data4 ordination')
 #Data has 801 samples, 20531 genes
 dim(data)
 #Store number of zero counts per gene
@@ -69,58 +73,73 @@ data4 <- cbind(data[,'sample'],data3)
 colnames(data4)[1] <- 'sample'
 #Bind the tumor class
 data4 <- inner_join(label,data4)
-
+toc()
 
 #### Ordination #### 
 
 ## PCA
-
+tic('PCA ordination')
 PCA <- prcomp(data4[,-c(1:2)],center = TRUE, scale. = TRUE)
 # Select the variables that capture  ~0.95 of the variance
 V.explained <- cumsum(PCA$sdev^2/sum(PCA$sdev^2))
 PCA_data <- data.frame(PCA$x[,which(V.explained <= 0.95)])
 # Add sample data back to the PCA data
 PCA_data <- cbind(label,PCA_data)
+toc()
 
 #convex hull for plot
 hull_PCA <- PCA_data %>% group_by(Class) %>% slice(chull(PC1,PC2))
 # Create a plot using the first two PCs
 # PCA_plot <- autoplot(PCA, data = data4, colour = "Class", frame = TRUE)
-PCA_plot <- ggplot(data = PCA_data, aes(x = PC1,y = PC2)) + aes(color = Class, fill = Class)+ geom_point() + geom_polygon(data = hull_PCA, alpha = 0.4) + labs(title = 'Clustering of PC1 and PC2')
-PCA_plot
+PCA_plot <- ggplot(data = PCA_data, aes(x = PC1,y = PC2)) + aes(color = Class, fill = Class)+ geom_point() + geom_polygon(data = hull_PCA, alpha = 0.4) 
+
 
 
 ## UMAP
 
+tic('UMAP ordination, 2 dims')
 # Umap with two dimensions
 UMAP <- umap(data4[,-c(1:2)])
 UMAP_data <- data.frame(cbind(label,UMAP$layout))
+toc()
 
+tic('UMAP ordination, 10 dims')
 # Umap with 10 dimensions
 UMAP_10 <- umap(data4[,-c(1:2)], n_components = 10)
 UMAP_10_data <- data.frame(cbind(label,UMAP_10$layout))
+toc()
 
+tic('UMAP ordination, PCA dims')
 # Umap with the same number of dimensions as PCA_data
 UMAP_nPCA <- umap(data4[,-c(1:2)], n_components = length(which(V.explained <= 0.95)))
 UMAP_nPCA_data <- data.frame(cbind(label,UMAP_nPCA$layout))
+toc()
 
 #convex hull for plot
 hull_UMAP <- UMAP_data %>% group_by(Class) %>% slice(chull(X1,X2))
 # Create a plot using the first two Axis
-UMAP_plot <- ggplot(data = UMAP_data, aes(x = X1,y = X2)) + aes(color = Class, fill = Class)+ geom_point() + geom_polygon(data = hull_UMAP, alpha = 0.4) + labs(title = 'Clustering after UMAP ordination')
-UMAP_plot
+UMAP_plot <- ggplot(data = UMAP_data, aes(x = X1,y = X2)) + aes(color = Class, fill = Class)+ geom_point() + geom_polygon(data = hull_UMAP, alpha = 0.4) + labs(x = 'UMAP1', y = 'UMAP2')
+
 
 ## tSNE
 
+tic('tSNE ordination')
 # theta = 0.0 is an exact tSNE
 tSNE <- Rtsne(as.matrix(data4[,-c(1:2)]), theta=0.0)
 tSNE_data <- cbind(label,tSNE$Y)
+toc()
 
 #convex hull for plot
 hull_tSNE <- tSNE_data %>% group_by(Class) %>% slice(chull(`1`,`2`))
 # Create a plot using the first two Axis
-tSNE_plot <- ggplot(data = tSNE_data, aes(x = `1`,y = `2`)) + aes(color = Class, fill = Class) + geom_point()+ geom_polygon(data = hull_tSNE, alpha = 0.4) + labs(title = 'Clustering after tSNE ordination')
-tSNE_plot
+tSNE_plot <- ggplot(data = tSNE_data, aes(x = `1`,y = `2`)) + aes(color = Class, fill = Class) + geom_point()+ geom_polygon(data = hull_tSNE, alpha = 0.4) + labs(x = 'tSNE1', y = 'tSNE2')
+
+
+## Figure of ordination plots
+figure <- ggarrange(PCA_plot, UMAP_plot, tSNE_plot, labels = c('A', 'B', 'C'), common.legend = TRUE, legend = 'right')
+annotate_figure(figure, top = text_grob('Clustering of data after implementing different ordination methods \n', face = 'bold', size = 18))
+
+
 
 #### Train Test Splitting ####
 
@@ -132,25 +151,36 @@ Train <- createDataPartition(data4$Class, p=0.9, list=FALSE)
 #### Model fitting and cross validation within the test set ####
 
 # 10-fold cross validation
+tic('CV data4')
 cv_data4 <- cv.glmnet(as.matrix(data4[Train,-c(1,2)]), factor(data4[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                       lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                       type.multinomial="grouped", nfolds=10)
+toc()
+tic('CV PCA')
 cv_PCA <- cv.glmnet(as.matrix(PCA_data[Train,-c(1,2)]), factor(PCA_data[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                     lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                     type.multinomial="grouped", nfolds=10)
+toc()
+tic('CV UMAP2')
 cv_UMAP <- cv.glmnet(as.matrix(UMAP_data[Train,-c(1,2)]), factor(UMAP_data[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                      lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                      type.multinomial="grouped", nfolds=10)
+toc()
+tic('CV UMAP10')
 cv_UMAP_10 <- cv.glmnet(as.matrix(UMAP_10_data[Train,-c(1,2)]), factor(UMAP_10_data[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                      lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                      type.multinomial="grouped", nfolds=10)
+toc()
+tic('CV UMAP_nPCA')
 cv_UMAP_nPCA <- cv.glmnet(as.matrix(UMAP_nPCA_data[Train,-c(1,2)]), factor(UMAP_nPCA_data[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                      lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                      type.multinomial="grouped", nfolds=10)
+toc()
+tic('CV tSNE')
 cv_tSNE <- cv.glmnet(as.matrix(tSNE_data[Train,-c(1,2)]), factor(tSNE_data[Train,c(2)]), family="multinomial", alpha=.93, thresh = 1e-07,
                      lambda = c(0.1,0.05,0.01,0.005,0.001, 0.0005, 0.0001),
                      type.multinomial="grouped", nfolds=10)
-
+toc()
 
 #### Predicting on validation data ####
 
@@ -167,24 +197,49 @@ validate <- function(model,data,train_set){
   
 }
 
+tic('Val Data4')
 val_data4 <- validate(cv_data4,data4,Train)
+toc()
+tic('Val PCA')
 val_PCA <- validate(cv_PCA,PCA_data,Train)
+toc()
+tic('Val UMAP2')
 val_UMAP <- validate(cv_UMAP,UMAP_data,Train)
+toc()
+tic('Val UMAP10')
 val_UMAP_10 <- validate(cv_UMAP_10,UMAP_10_data,Train)
+toc()
+tic('Val UMAP_nPCA')
 val_UMAP_nPCA <- validate(cv_UMAP_nPCA,UMAP_nPCA_data,Train)
+toc()
+tic('Val tSNE')
 val_tSNE <- validate(cv_tSNE,tSNE_data,Train)
-
+toc()
 
 #### Evaluations ####
 
-
-# function to check how many the model got right, if its 0 all were classified correctly - I know this code could be cleaner...
+## Function to check how many the model got right, if its 0 all were classified correctly - I know this code could be cleaner...
 length(which(val_data4$Class==val_data4$Predicted)) - length(val_data4$Class)
 length(which(val_PCA$Class==val_PCA$Predicted)) - length(val_PCA$Class)
 length(which(val_UMAP$Class==val_UMAP$Predicted)) - length(val_UMAP$Class)
 length(which(val_UMAP_10$Class==val_UMAP_10$Predicted)) - length(val_UMAP_10$Class)
 length(which(val_UMAP_nPCA$Class==val_UMAP_nPCA$Predicted)) - length(val_UMAP_nPCA$Class)
 length(which(val_tSNE$Class==val_tSNE$Predicted)) - length(val_tSNE$Class)
+
+## Time complexity of different classifiers 
+times <- read.csv('/homes/anovak9/stats/Stats/times.csv', header = FALSE, stringsAsFactors = FALSE)
+rownames(times) <- c('model', 'ordination', 'cv', 'val', 'tot_runtime')
+times <- times[,2:7]
+times
+#Only use total runtimes for plot
+tot_time <- times[c(1,5),]
+tot_time <- as.data.frame(t(tot_time))
+tot_time
+#Plot
+ggplot(tot_time, aes(x = model, y = tot_runtime))+
+  geom_bar(stat = 'identity', fill = 'deepskyblue2', width = 0.5, col = 'black')+
+  labs(title = 'Total run time of each classifier', x = '\n Classifier Type', y = 'Time (sec) \n')+
+  theme(axis.text.x = element_text(size = 14), axis.title = element_text(size = 16), plot.title = element_text(size = 18, face = 'bold', hjust = 0.5), axis.text.y = element_text(size = 13))
 
 
 #### ROC Curves ####
